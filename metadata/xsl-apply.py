fdc5a561b62835ece73dac612d1565390fd5d340
streino <streino@disroot.org>
2024-07-03 14:40:27 +0200

    Add lxml processor

--- metadata/xsl-apply.py
+++ metadata/xsl-apply.py
@@ -1,14 +1,28 @@
 import sys
+from lxml import etree
 from saxonche import PySaxonProcessor
 
-xsl_file = sys.argv[1]
-xml_file = sys.argv[2]
+proc = sys.argv[1]
+xsl_file = sys.argv[2]
+xml_file = sys.argv[3]
 
-with PySaxonProcessor(license=False) as saxon_proc:
+if proc == 'lxml':
+  xsl = etree.parse(xsl_file)
+  transform = etree.XSLT(xsl)
+
+  iso = etree.parse(xml_file)
+  dcat = transform(iso, CoupledResourceLookUp="'disabled'")
+  print(etree.tostring(dcat, pretty_print=True, encoding='unicode'))
+
+elif proc == 'saxon':
+  with PySaxonProcessor(license=False) as saxon_proc:
     xslt_proc = saxon_proc.new_xslt30_processor()
 
-xslt = xslt_proc.compile_stylesheet(stylesheet_file=xsl_file)
-xslt.set_property('!indent', 'yes')
+  xslt = xslt_proc.compile_stylesheet(stylesheet_file=xsl_file)
+  xslt.set_property('!indent', 'yes')
 
-output = xslt.transform_to_string(source_file=xml_file)
-print(output)
+  output = xslt.transform_to_string(source_file=xml_file)
+  print(output)
+
+else:
+  print("Unknown processor:", proc)
